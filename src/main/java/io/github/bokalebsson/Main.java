e8bedc4cae5c4e471e43563eea2c82d4f34a729e
BoKalebsson <bo_kalebsson@hotmail.com>
2025-06-12 10:13:57 +0200

    Added demand list for the project.

--- src/main/java/io/github/bokalebsson/Main.java
+++ src/main/java/io/github/bokalebsson/Main.java
@@ -3,5 +3,34 @@ package io.github.bokalebsson;
 public class Main {
     public static void main(String[] args) {
 
+      /* List of demands:
+       * - Maven project: check!
+       *
+       * Calculator should support:
+       * - Addition:
+       * - Subtraction:
+       * - Multiplication:
+       * - Division:
+       *
+       * User input demands:
+       * - Enter the numbers:
+       * - Enter the operand:
+       * - Ask user to make another calculation or exit:
+       *
+       * Looping demands:
+       * - Continue running until user ends the calculator:
+       *
+       * Optional demands:
+       * - Subtraction and Addition should be able to process several numbers:
+       * - Verify user inputs and handle invalid inputs:
+       * - JUnit tests:
+       * */
+
+
+
+
+
+
+
     }
 }
